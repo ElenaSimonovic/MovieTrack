4fdc7fdaf97a8ad8e09cbcc1dfc714a5f50a5660
lmajetic <lukamajeticc@gmail.com>
2026-04-26 15:55:07 +0200

    Prepravak korisnik.routes

--- backend/routes/korisnik.routes.js
+++ backend/routes/korisnik.routes.js
@@ -5,6 +5,6 @@ const korisnikController = require('../controllers/korisnik.controller');
 router.post('/registracija', korisnikController.registracija);
 router.post('/prijava', korisnikController.prijava);
 router.delete('/:email', korisnikController.obrisiKorisnika);
-router.put('/blokiraj/:email', korisnik.blokiraj);
+router.put('/blokiraj/:email', korisnikController.blokiraj);
 
 module.exports = router;
